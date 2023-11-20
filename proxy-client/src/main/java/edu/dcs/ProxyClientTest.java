19e860ade2afe266394701273bfd7b6f2e747202
Bipul Singh <bipul392ssnl@gmail.com>
2023-11-20 14:24:41 -0500

    Initial commit proxy client

---
+++ proxy-client/src/main/java/edu/dcs/ProxyClientTest.java
@@ -0,0 +1,47 @@
+package com.proxy.bloom.proxyserver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.SocketException;
+
+public class ProxyClientTest {
+    public static void main(String[] args) {
+        String proxyHost = "localhost"; // Change this to the address of your proxy server
+        int proxyPort = 8082; // Change this to the port of your proxy server
+
+        while (true) {
+            try {
+                Socket proxySocket = new Socket(proxyHost, proxyPort);
+
+                PrintWriter proxyWriter = new PrintWriter(proxySocket.getOutputStream(), true);
+                proxyWriter.println("GET https://www.javatpoint.com/simple-html-pages HTTP/1.1");
+                proxyWriter.println(); // Blank line indicates end of headers
+
+                BufferedReader proxyReader = new BufferedReader(new InputStreamReader(proxySocket.getInputStream()));
+
+                // String responseLine;
+                while ((proxyReader.readLine()) != null) {
+                    // System.out.println("Proxy Response: " + responseLine);
+                    System.out.println("Successfully fetched the response");
+                }
+
+                proxySocket.close();
+
+            } catch (SocketException se) {
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                continue;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            break;
+        }
+    }
+}
+
---
+++ proxy-client/src/main/java/edu/dcs/RoundRobinIterator.java
@@ -0,0 +1,35 @@
+package com.proxy.bloom.proxyserver;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class RoundRobinIterator<T> implements Iterable<T> {
+
+    List<T> coll;
+
+    public RoundRobinIterator(List<T> coll) {
+        this.coll = coll;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+
+            int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public T next() {
+                T server = coll.get(index);
+                index = (index + 1) % coll.size();
+                return server;
+            }
+
+        };
+    }
+
+}
